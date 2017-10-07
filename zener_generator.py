7dc30125a0af51299cd9c89ab6fb0c5bcf40d834
Nicolas Mauthes <nmauthes@netscape.net>
2017-10-07 10:36:15 -0700

    rewrote zener generator; now use .jpg images

--- zener_generator.py
+++ zener_generator.py
@@ -1,43 +1,24 @@
 import os, sys
 import random
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageOps
 
-offset = 4.5 # Should not be zero
+MAX_SIZE_OFFSET = 10
+MAX_POS_OFFSET = 5
+MAX_ROTATION = 360
+MAX_NOISE_PROBABILITY = 0.5
 
-def draw_circle(im, rotation=0, stroke=1):
-    shape = Image.new('1', (im.size[0], im.size[1]), 0)
-    x_off, y_off = shape.size[0] / offset, shape.size[1] / offset
+def draw_shape(bg, shape, pos_offset=0, size_offset=0, rotation=0):
+    file_path = os.path.join(os.getcwd(), 'zener_shapes', shape + '.jpg')
 
-    draw = ImageDraw.Draw(shape)
-    draw.ellipse((x_off, y_off) + (shape.size[0] - x_off, shape.size[1] - y_off), fill=0, outline=1)
-    del draw
+    try:
+        src = Image.open(file_path)
+    except IOError:
+        print 'Shape not found'
 
-    im.paste(0, mask=shape.rotate(0))
+    mask = ImageOps.invert(src).convert('1').rotate(rotation).resize((bg.size[0] - size_offset, bg.size[1] - size_offset))
 
-def draw_cross(im, rotation=0, stroke=1):
-    shape = Image.new('1', (im.size[0], im.size[1]), 0)
-    x_off, y_off = shape.size[0] / offset, shape.size[1] / offset
-
-    draw = ImageDraw.Draw(shape)
-    draw.line((shape.size[0] / 2., y_off) + (shape.size[0] / 2., shape.size[1] - y_off), fill=1, width=stroke)
-    draw.line((x_off, shape.size[0] / 2.) + (shape.size[0] - x_off, shape.size[1] / 2.), fill=1, width=stroke)
-    del draw
-
-    im.paste(0, mask=shape.rotate(rotation))
-
-def draw_square(im, rotation=0, stroke=1):
-    shape = Image.new('1', (im.size[0], im.size[1]), 0)
-    x_off, y_off = shape.size[0] / offset, shape.size[1] / offset
-
-    draw = ImageDraw.Draw(shape)
-    draw.line((x_off, y_off) + (shape.size[0] - x_off, y_off), fill=1, width=stroke)
-    draw.line((shape.size[0] - x_off, y_off) + (shape.size[0] - x_off, shape.size[1] - y_off), fill=1, width=stroke)
-    draw.line((shape.size[0] - x_off, shape.size[1] - y_off) + (x_off, shape.size[1] - y_off), fill=1, width=stroke)
-    draw.line((x_off, shape.size[1] - y_off) + (x_off, y_off), fill=1, width=stroke)
-    del draw
-
-    im.paste(0, mask=shape.rotate(rotation))
+    bg.paste(0, box=((bg.size[0] - mask.size[0]) / 2 + pos_offset, (bg.size[1] - mask.size[1]) / 2 + pos_offset), mask=mask)
 
 def generate_zener_cards(folder_name, num_examples):
     path = os.path.join(os.getcwd(), folder_name)
@@ -48,21 +29,18 @@ def generate_zener_cards(folder_name, num_examples):
         for filename in os.listdir(path):
             os.remove(os.path.join(path, filename))
 
-    for i in range(0, num_examples):
+    shapes = ['O', 'P', 'Q', 'S', 'W']
+    for n in range(0, num_examples):
         card = Image.new('1', (25, 25), 1)
 
-        choice = random.randint(1, 3)
-        if choice == 1:
-            draw_circle(card)
-            card_type = 'O'
-        elif choice == 2:
-            draw_cross(card)
-            card_type = 'P'
-        else:
-            draw_square(card)
-            card_type = 'Q'
+        size_offset = random.randint(0, MAX_SIZE_OFFSET)
+        pos_offset = random.randint(-MAX_POS_OFFSET, MAX_POS_OFFSET)
+        rotation = random.randint(0, MAX_ROTATION)
 
-        filename = '{}_{}.png'.format(i + 1, card_type)
+        shape = random.choice(shapes)
+        draw_shape(card, shape, pos_offset=pos_offset, size_offset=size_offset, rotation=rotation)
+
+        filename = '{}_{}.png'.format(n + 1, shape)
         card.save(os.path.join(path, filename))
 
 if len(sys.argv) != 3:
