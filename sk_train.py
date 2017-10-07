b228abc8605c4c7d5bfc74335264f7f942bee0be
sordonia120446 <samuel.ordonia@gmail.com>
2017-10-07 16:13:06 -0700

    add poly kernel

---
+++ sk_train.py
@@ -0,0 +1,18 @@
+"""
+Train an SVM on a polynomial-kernel transformed dataset.
+
+:authors Jason, Nick, Sam
+"""
+
+import numpy as np
+
+
+def poly_kernel(x, x_i, p=4, c=1):
+    """
+    :param x_t: training input
+    :param x_i: input vector
+    :returns type int:
+    """
+    x_t = np.transpose(x)
+    return (np.dot(x_t, x_i) + c)**p
+
