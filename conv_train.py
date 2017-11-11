448508e3807087c9e92b9bd24cc23a853b57a416
sordonia120446 <samuel.ordonia@gmail.com>
2017-11-11 15:41:02 -0800

    add reminder note for output layer dim

--- conv_train.py
+++ conv_train.py
@@ -61,7 +61,7 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.fc2 = nn.Linear(50, 10)  # TODO change 10 to 4
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
