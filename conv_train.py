9cbb204aee84bdb2cabf8f128ac71e1b5d067782
sordonia120446 <samuel.ordonia@gmail.com>
2017-11-12 22:14:19 -0800

    pull in core logic into main call

--- conv_train.py
+++ conv_train.py
@@ -12,47 +12,6 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
-# Training settings
-parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                    help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                    help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                    help='learning rate (default: 0.01)')
-parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                    help='SGD momentum (default: 0.5)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-
-torch.manual_seed(args.seed)
-if args.cuda:
-    torch.cuda.manual_seed(args.seed)
-
-
-kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=True, download=True,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])),
-    batch_size=args.test_batch_size, shuffle=True, **kwargs)
-
 
 class Net(nn.Module):
     def __init__(self):
@@ -72,16 +31,6 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x)
 
-model = Net()
-if args.cuda:
-    model.cuda()
-
-optimizer = optim.SGD(
-    model.parameters(),
-    lr=args.lr,
-    momentum=args.momentum,
-    weight_decay=0  # L2 penalty value
-)
 
 def train(epoch):
     model.train()
@@ -98,6 +47,7 @@ def train(epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data[0]))
+
 
 def test():
     model.eval()
@@ -118,20 +68,34 @@ def test():
         100. * correct / len(test_loader.dataset)))
 
 
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    test()
-
 ############################################################
 #CLARGS
 ############################################################
-# parser = argparse.ArgumentParser(
-#     description='CNN for zener card classification',
-#     formatter_class=argparse.RawDescriptionHelpFormatter,
-#     epilog='For further questions, please consult the README.'
-# )
+parser = argparse.ArgumentParser(
+    description='CNN for zener card classification',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    epilog='For further questions, please consult the README.'
+)
 
-# # Add CLARGS
+# Training settings
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+
+# Add positional CLARGS
 # parser.add_argument(
 #     'cost',
 #     help='Specify the cost function [cross, cross-l1, cross-l2].'
@@ -145,11 +109,11 @@ for epoch in range(1, args.epochs + 1):
 #     type=float,
 #     help='Epsilon error tolerance.'
 # )
-# parser.add_argument(
-#     'max_updates',
-#     type=int,
-#     help='Training epochs.'
-# )
+parser.add_argument(
+    'max_updates',
+    type=int,
+    help='Number of training epochs.'
+)
 # parser.add_argument(
 #     'class_letter',
 #     help='Specify the class letter [P, W, Q, S].'
@@ -164,5 +128,43 @@ for epoch in range(1, args.epochs + 1):
 # )
 
 
-# if __name__ == '__main__':
-#     args = parser.parse_args()
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    # CUDA setup
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    torch.manual_seed(args.seed)
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+    kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
+    model = Net()
+    if args.cuda:
+        model.cuda()
+
+    optimizer = optim.SGD(
+        model.parameters(),
+        lr=args.lr,
+        momentum=args.momentum,
+        weight_decay=0  # L2 penalty value
+    )
+
+    # Data import
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                    transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.1307,), (0.3081,))
+                    ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.1307,), (0.3081,))
+                    ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+    # Train & test per epoch
+    for epoch in range(1, args.max_updates + 1):
+        train(epoch)
+        test()
