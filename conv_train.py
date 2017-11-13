86bc7b3d3ecb0ca328d6863d2e3e1cd2ae901f25
sordonia120446 <samuel.ordonia@gmail.com>
2017-11-12 22:23:25 -0800

    remove random seed & epoch from opt args

--- conv_train.py
+++ conv_train.py
@@ -12,6 +12,8 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
+RANDOM_SEED = 1
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -82,16 +84,12 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                     help='SGD momentum (default: 0.5)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
 
@@ -133,9 +131,9 @@ if __name__ == '__main__':
 
     # CUDA setup
     args.cuda = not args.no_cuda and torch.cuda.is_available()
-    torch.manual_seed(args.seed)
+    torch.manual_seed(RANDOM_SEED)
     if args.cuda:
-        torch.cuda.manual_seed(args.seed)
+        torch.cuda.manual_seed(RANDOM_SEED)
     kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
     model = Net()
