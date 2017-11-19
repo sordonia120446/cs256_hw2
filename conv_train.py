342dc06b1779f46330509eb6a5dd422bbfbccba4
sordonia120446 <samuel.ordonia@gmail.com>
2017-11-18 16:39:25 -0800

    switch to tf

--- conv_train.py
+++ conv_train.py
@@ -1,256 +1,344 @@
 """
-CNN for zener card classification.
+Training in tensorflow.
+Build a classifier for "alien DNA."  They have six possible labels:
+    NONSTICK
+    12-STICKY
+    34-STICKY
+    56-STICKY
+    78-STICKY
+    STICK_PALINDROME 
 
 :authors Jason, Nick, Sam
 """
 
 import argparse
+import glob
+import os
+import time
 
-import torch
-from torch.utils.data import Dataset, DataLoader
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import transforms
-from torch.autograd import Variable
-
-from utils import init_data
-
-##################################################################################################
-# CNN
-##################################################################################################
-
-# Constants
-RANDOM_SEED = 1
+import numpy as np
+import tensorflow as tf
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)  # TODO change 10 to 4
+tf.logging.set_verbosity(tf.logging.INFO)
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x)
+label_map = {
+    'NONSTICK': 0,
+    '12-STICKY': 1,
+    '34-STICKY': 2,
+    '56-STICKY': 3,
+    '78-STICKY': 4,
+    'STICK_PALINDROME': 5
+}
+
+MINIBATCH_SIZE = 100
 
 
-def train(epoch, train_loader):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        print(f'Data: {type(data)}')
-        print(f'Target: {type(target)}')
-        print(target)
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data), Variable(target)
-        print(f'Data: {data}')
-        print(f'Target: {target}')
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+def convert_data(input_line):
+    """
+    Converts the data from raw DNA to ASCII char numpy array.
+
+    :param input_line: line of input data (DNA & label)
+    :returns type <numpy arr>: tf-friendly input vector
+    """
+    
+    try:
+        data, label = input_line.split(",")
+    except ValueError:
+        raise Exception('Check the input file format')
+
+    data.strip()  # remove whitespace
+    label.strip()  # remove whitespace
+
+    # Convert letters into ASCII
+    data = [ord(c) for c in data]
+
+    # Map labels to ints
+    try:
+        label = label_map[label]
+    except KeyError:
+        raise Exception(f'Check spelling on label {label}')
+
+    return np.array(data, dtype=np.float32), label
 
 
-def test(test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    for data, target in test_loader:
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-        output = model(data)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
-        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+def load_data(data_folder):
+    """
+    Iterate through each file in data_folder and construct
+    the input data features (40-len DNA).
 
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    :param data_folder: path to folder of data
+    :param exclude: kth file to exclude in cross-validation
+    :returns type list: list of dicts for DNA-arr & label
+    """
+    
+    data_files = os.path.join(data_folder, '*.txt')
+    data = []
+    for f_path in glob.glob(data_files):
 
-def parse_network_description(network_description):
+        with open(f_path) as f_in:
+            for line in f_in.read().splitlines():
+                dna_arr, label = convert_data(line)
+                data.append({
+                    'x': dna_arr,
+                    'y': label
+                })
+
+    return data
+
+
+def get_confusion_matrix(labels, predictions):
     '''
-    Parse the file containing the network description and return a set of number to be used when
-    constructing CNN
+    Generate a confusion matrix for evaluation.
 
-    :param network_description: The path to the file containing the network description
-    :return: A 2D list where each index is structured as: [#_layers feature_map] or [#_layers]
+    Note for rows, columns 0...5 0=NONSTICK, 1=12-STICKY...up to 5=STICK_PALINDROME
+
+    :param labels: A tensor containing actual labels for each example
+    :param predictions: A tensor containing the label predicted by the model
+    :return: Tuple containing matrix and update op for use in eval_metric_ops
     '''
+    with tf.variable_scope('get_confusion_matrix'):
+        matrix = tf.confusion_matrix(labels=labels, predictions=predictions, num_classes=6)
+        matrix_sum = tf.Variable(tf.zeros(shape=(6,6), dtype=tf.int32),
+                                 trainable=False,
+                                 name='confusion_matrix',
+                                 collections=[tf.GraphKeys.LOCAL_VARIABLES])
 
-    net_desc = []
-    with open(network_description, 'r') as f:
-        for line in f.read().splitlines():
-            net_desc.append([int(x) for x in line.split(' ')])
+        # Update matrix_sum by adding matrix to it
+        update = tf.assign_add(matrix_sum, matrix)
 
-    return net_desc
-
-
-##################################################################################################
-# Data Processing
-##################################################################################################
-
-
-class ZenerDataset(Dataset):
-
-    def __init__(self, args, train=True, k_fold=1, transform=None, target_transform=None):
-        self.args = args
-        self.train = train
-        self.k_fold = k_fold  # split the training & test data    
-        self.transform = transform
-        self.target_transform = target_transform
-
-        input_data = init_data(args, as_PIL=True)
-
-        if self.train:
-            self.train_data = input_data['X_plus'] + input_data['X_minus']
-            self.train_labels = input_data['Y_plus'] + input_data['Y_minus']
-        else:
-            self.test_data = input_data['X_plus'] + input_data['X_minus']
-            self.test_labels = input_data['Y_plus'] + input_data['Y_minus']
+        # Return confusion matrix and update op
+        return tf.convert_to_tensor(matrix_sum), update
 
 
-    def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
+def dnn_model_fn(features, labels, mode):
+    """
+    Multi-layer DNN with dense hidden layers & relu activation.
 
-        Returns:
-            tuple: (image, target) where target is index of the target class.
-        """
-        if self.train:
-            img, target = self.train_data[index], self.train_labels[index]
-        else:
-            img, target = self.test_data[index], self.test_labels[index]
+    :param features: the features to train/test on
+    :param labels: the labels on each DNA to check against prediction
+    :param mode: can be train, 5fold, or test
+    :returns type tf: tf goodness
+    """
 
-        if self.transform is not None:
-            img = self.transform(img)
+    # Shape input layer
+    l0 = tf.reshape(features['x'], [-1, 40])
 
-        if self.target_transform is not None:
-            target = self.target_transform(target)
+    # Define hidden layers
+    l1 = tf.layers.dense(inputs=l0, units=40, activation=tf.nn.relu)
+    l2 = tf.layers.dense(inputs=l1, units=40, activation=tf.nn.relu)
+    l3 = tf.layers.dense(inputs=l2, units=40, activation=tf.nn.relu)
+    l4 = tf.layers.dense(inputs=l3, units=40, activation=tf.nn.relu)
 
-        return img, target
+    # Define output (logit) layer
+    logits = tf.layers.dense(inputs=l4, units=6)
 
-    def __len__(self):
-        if self.train:
-            return len(self.train_data)
-        else:
-            return len(self.test_data)
+    predictions = {
+        "classes": tf.argmax(input=logits, axis=1),
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=6)
+    loss = tf.losses.softmax_cross_entropy(
+        onehot_labels=onehot_labels,
+        logits=logits
+    )
+
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels,
+            predictions=predictions["classes"]
+        ),
+        "confusion_matrix": get_confusion_matrix(labels, predictions["classes"])
+    }
+
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        loss=loss,
+        eval_metric_ops=eval_metric_ops
+    )
 
 
-##################################################################################################
-# CLARGS
-##################################################################################################
+def train_mode(data, model_name):
+    # Init estimator
+    model_dir = os.path.join(model_name)
+    sticky_classifier = tf.estimator.Estimator(
+        model_fn=dnn_model_fn,
+        model_dir=model_dir
+    )
+
+    # Set up logging for predictions
+    # Log the values in the "Softmax" tensor with label "probabilities"
+    tensors_to_log = {"probabilities": "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log,
+        every_n_iter=1000
+    )
+
+    features = np.asarray([d['x'] for d in data])
+    labels = np.asarray([d['y'] for d in data], dtype=np.float32)
+
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={'x': features},
+        y=labels,
+        batch_size=MINIBATCH_SIZE,
+        num_epochs=None,
+        shuffle=True
+    )
+
+    start = time.perf_counter()
+    sticky_classifier.train(
+        input_fn=train_input_fn,
+        steps=20000,
+        hooks=[logging_hook]
+    )
+    train_time = time.perf_counter() - start
+    print(f'Trained for {train_time:.3f} seconds')
+
+
+def test_mode(data, model_name):
+    # Init estimator
+    model_dir = os.path.join(model_name)
+    sticky_classifier = tf.estimator.Estimator(
+        model_fn=dnn_model_fn,
+        model_dir=model_dir
+    )
+    features = np.asarray([d['x'] for d in data])
+    labels = np.asarray([d['y'] for d in data], dtype=np.float32)
+
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": features},
+        y=labels,
+        num_epochs=1,
+        shuffle=False
+    )
+
+    start = time.perf_counter()
+    eval_results = sticky_classifier.evaluate(input_fn=eval_input_fn)
+    test_time = time.perf_counter() - start
+    print(f'Tested for {test_time:.3f} seconds')
+    return eval_results['accuracy']
+
+
+def main(args):
+    # call function based on mode
+    data = load_data(args.data_folder)
+    model_name = args.model_file
+    if args.mode == 'train':
+        train_mode(data, model_name)
+        print('Processing complete!')
+        print(f'Total items trained on: {len(data)}')
+    elif args.mode == 'test':
+        test_mode(data, model_name)
+        print('Processing complete!')
+        print(f'Total items tested on: {len(data)}')
+    elif args.mode == '5fold':
+        k = 5
+        subset_size = int(len(data) / k)
+        subsets = [data[i:i + subset_size] for i in range(0, len(data), subset_size)]
+        # if size of data isn't divisible by 5, have a larger kth subset
+        if len(data) % k != 0:
+            subsets[k - 1] = subsets[k - 1] + subsets[k]
+            del subsets[k]
+        accuracy = 0
+        # perform cross validation
+        for i in range(k):
+            # exclude subset i for training data
+            subsets_copy = list(subsets)
+            del subsets_copy[i]
+            training_set = []
+            for subset in subsets_copy:
+                training_set += subset
+            # train on training_set
+            train_mode(training_set, model_name)
+            # test on subset i
+            test_set = subsets[i]
+            accuracy += test_mode(test_set, model_name)
+            print('Processing complete!')
+            print(f'Total items trained on: {len(training_set)}')
+            print(f'Total items trained on: {len(test_set)}')
+        print('Average 5fold accuracy: ', str(accuracy / 5.0))
+    else:
+        # debugging
+        # Init estimator
+        model_dir = os.path.join(model_name)
+        sticky_classifier = tf.estimator.Estimator(
+            model_fn=dnn_model_fn,
+            model_dir=model_dir
+        )
+
+        # Set up logging for predictions
+        # Log the values in the "Softmax" tensor with label "probabilities"
+        tensors_to_log = {"probabilities": "softmax_tensor"}
+        logging_hook = tf.train.LoggingTensorHook(
+            tensors=tensors_to_log,
+            every_n_iter=1000
+        )
+
+        features = np.asarray([d['x'] for d in data])
+        labels = np.asarray([d['y'] for d in data], dtype=np.float32)
+
+        train_input_fn = tf.estimator.inputs.numpy_input_fn(
+            x={'x': features},
+            y=labels,
+            batch_size=MINIBATCH_SIZE,
+            num_epochs=None,
+            shuffle=True
+        )
+        sticky_classifier.train(
+            input_fn=train_input_fn,
+            steps=20000,
+            hooks=[logging_hook]
+        )
+
+        # eval
+        eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+            x={"x": features},
+            y=labels,
+            num_epochs=1,
+            shuffle=False
+        )
+        eval_results = sticky_classifier.evaluate(input_fn=eval_input_fn)
+        print(eval_results)
+
+
+"""CLARGS"""
+
 parser = argparse.ArgumentParser(
-    description='CNN for zener card classification',
+    description='Train and test the neural net',
     formatter_class=argparse.RawDescriptionHelpFormatter,
     epilog='For further questions, please consult the README.'
 )
 
-# Training settings
-parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                    help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                    help='input batch size for testing (default: 1000)')
-parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                    help='learning rate (default: 0.01)')
-parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                    help='SGD momentum (default: 0.5)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA training')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-
-# Add positional CLARGS
-# parser.add_argument(
-#     'cost',
-#     help='Specify the cost function [cross, cross-l1, cross-l2].'
-# )
-# parser.add_argument(
-#     'network_description',
-#     help='Path to network description file.'
-# )
-# parser.add_argument(
-#     'epsilon',
-#     type=float,
-#     help='Epsilon error tolerance.'
-# )
 parser.add_argument(
-    'max_updates',
-    type=int,
-    help='Number of training epochs.'
-)
-parser.add_argument(
-    'class_letter',
-    help='Specify the class letter [P, W, Q, S].'
-)
-parser.add_argument(
-    'model_file_name',
-    help='Filename to output trained model.'
-)
-parser.add_argument(
-    'train_folder_name',
-    help='Locating of training data.'
+    'mode',
+    help='Specify whether to test or train as well as how to train'
 )
 
+parser.add_argument(
+    'model_file',
+    help='Path to the file containing the trained weights'
+)
+
+parser.add_argument(
+    'data_folder',
+    help='Path to the folder containing training/testing data'
+)
 
 if __name__ == '__main__':
     args = parser.parse_args()
+    main(args)
 
-    # CUDA setup
-    args.cuda = not args.no_cuda and torch.cuda.is_available()
-    torch.manual_seed(RANDOM_SEED)
-    if args.cuda:
-        torch.cuda.manual_seed(RANDOM_SEED)
-    kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-
-    # CNN setup
-    model = Net()
-    if args.cuda:
-        model.cuda()
-    optimizer = optim.SGD(
-        model.parameters(),
-        lr=args.lr,
-        momentum=args.momentum,
-        weight_decay=0  # L2 penalty value
-    )
-
-    # Data import
-    train_loader = torch.utils.data.DataLoader(
-        ZenerDataset(
-            args,
-            train=True,
-            transform=transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.1307,), (0.3081,))
-            ])),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
-
-    test_loader = torch.utils.data.DataLoader(
-        ZenerDataset(
-            args,
-            train=False,
-            transform=transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.1307,), (0.3081,))
-            ])),
-        batch_size=args.test_batch_size, shuffle=True, **kwargs)
-
-    # Train & test per epoch
-    for epoch in range(1, args.max_updates + 1):
-        train(epoch, train_loader)
-        test(test_loader)
